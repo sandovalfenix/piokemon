8f11fd97811f9afd54d09618ce8c45f408c7676d
sandovalfenix <sandovalfenix@gmail.com>
2025-12-04 22:17:16 -0500

    refactor: update routing to remove legacy zone components and introduce dynamic zone lobby

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,6 @@
 
 import { createRouter, createWebHistory } from 'vue-router';
 import WorldMapViewPage from '../views/WorldMapViewPage.vue';
-import ZoneGymCristoRey from '../views/ZoneGymCristoRey.vue';
-import ZoneParqueCana from '../views/ZoneParqueCana.vue';
-import ZoneZooCali from '../views/ZoneZooCali.vue';
-import ZonePlazoletaJairoVarela from '../views/ZonePlazoletaJairoVarela.vue';
-import ZoneLaErmita from '../views/ZoneLaErmita.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,29 +17,9 @@ const router = createRouter({
       component: WorldMapViewPage,
     },
     {
-      path: '/zona/la-ermita',
-      name: 'la-ermita',
-      component: ZoneLaErmita,
-    },
-    {
-      path: '/zona/zoologico-de-cali',
-      name: 'zoologico-de-cali',
-      component: ZoneZooCali,
-    },
-    {
-      path: '/zona/plazoleta-jairo-varela',
-      name: 'plazoleta-jairo-varela',
-      component: ZonePlazoletaJairoVarela,
-    },
-    {
-      path: '/zona/gimnasio-cristo-rey',
-      name: 'gym-cristo-rey',
-      component: ZoneGymCristoRey,
-    },
-    {
-      path: '/zona/parque-de-la-cana',
-      name: 'Parque-de-la-Cana',
-      component: ZoneParqueCana,
+      path: '/zone-lobby/:zoneName',
+      name: 'zone-lobby',
+      component: import('../views/ZoneLobbyView.vue'),
     },
   ],
 });
